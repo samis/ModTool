6b54830061d06e90e9789b70c3371e855edcb32e
CompanionCube <samuel.hodgkins@sky.com>
2014-02-02 20:27:10 +0000

    Initial Commit

---
+++ src/in/cloudns/companion/ModTool/ModTool.java
@@ -0,0 +1,24 @@
+package in.cloudns.companion.ModTool;
+
+public class ModTool {
+
+	/**
+	 * Start and run ModTool
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		initialise();
+		run();
+	}
+
+	private static void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void initialise() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
