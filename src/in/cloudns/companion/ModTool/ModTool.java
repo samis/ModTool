2e2d9206396e39dca1e44c2f29432993e2a40883
CompanionCube <samuel.hodgkins@sky.com>
2014-02-02 21:40:11 +0000

    Improved loggin format. Now more informative.

--- src/in/cloudns/companion/ModTool/ModTool.java
+++ src/in/cloudns/companion/ModTool/ModTool.java
@@ -1,30 +1,31 @@
 package in.cloudns.companion.ModTool;
+
 import org.apache.log4j.*;
 
 public class ModTool {
 
-	/**
-	 * Start and run ModTool
-	 * @param args
-	 */
-	static final Logger logger = Logger.getLogger(ModTool.class);
-	public static void main(String[] args) {
-		BasicConfigurator.configure();
-		logger.debug("Logger configured");
-		ModTool modtool = new ModTool();
-		modtool.initialise();
-		logger.info("Loading complete. Running ModTool");
-		modtool.run();
-	}
+    /**
+     * Start and run ModTool
+     * 
+     * @param args
+     */
+    static final Logger logger = Logger.getLogger(ModTool.class);
 
-	private void run() {
-		
-		
-	}
+    public static void main(String[] args) {
+	logger.debug("Logger configuration loaded");
+	ModTool modtool = new ModTool();
+	modtool.initialise();
+	logger.info("Loading complete. Running ModTool");
+	modtool.run();
+    }
 
-	private void initialise() {
-		logger.info("ModTool is loading. This shouldn't take long.");
-		
-	}
+    private void run() {
+
+    }
+
+    private void initialise() {
+	logger.info("ModTool is loading. This shouldn't take long.");
+
+    }
 
 }
